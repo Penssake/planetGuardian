c50cd1eeb1c6d760ccf9d47b400e66680410b447
Christina Thomas <Penssake@gmail.com>
2017-07-17 16:32:08 -0600

    First commit. Server set-up

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const fs = require('fs');
+const pg = require('pg');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.use(express.static('./public'));
+
+app.get('/index', (req, res) => {
+  res.sendFile('index.html', { root: './public' });
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('.'));
+
+app.listen(PORT, () => console.log(`Your server is now running on port ${PORT}`));

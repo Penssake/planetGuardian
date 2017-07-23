35f81623c525b033fab42950063cd93ab23423bf
Christina Thomas <Penssake@gmail.com>
2017-07-22 19:12:44 -0600

    Internet Problems D:

---
+++ public/scripts/views/characterSlide.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// (function(module) {
+//   function Character(charactersData) {
+//     Object.keys(charactersData).forEach(key => [key] = )
+//   }
+// })
+
+(function(module) {
+
+let registrationForm = document.getElementById('form');
+registrationForm.submit(preventDefault(), DisplayCharacter());
+
+function DisplayCharacter() {
+    console.log('You did it!');
+  }
+});

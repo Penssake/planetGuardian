4d29eb2e62960feaa88d637c9371be3fd82213cb
Christina Thomas <Penssake@gmail.com>
2017-07-27 00:49:43 -0600

    handlebars template added/refactoring js

--- public/scripts/views/characterSlide.js
+++ public/scripts/views/characterSlide.js
@@ -8,11 +8,24 @@
 
 (function(module) {
 
-  let charactersArray = [];
-  $.getJSON('/data/characters.json', function(characters) {
-    characters.forEach(function(charactersDataObject) {
-      charactersArray.push(characters);
-      console.log(charactersArray);
+  function DisplayCharacter(characters) {
+    charactersArray.name = name;
+    charactersArray.img = img;
+    charactersArray.description = description;
+    // var currentCharacter = [];
+    // currentCharacter.push(this);
+    console.log('hey');
+  };
+
+  DisplayCharacter.prototype.toHTML = function() {
+    let renderDisplayCharacter = Handlebars.compile($('#entry-template').text());
+    return renderDisplayCharacter(this);
+  }
+
+  $.getJSON('/data/characters.json', function(charactersData) {
+    charactersData.map(function(charactersObject) {
+      let characters = new DisplayCharacter(charactersObject);
+      $('#characterSlot').append(characters.toHTML());
       // DisplayCharacter(charactersObj);
     });
   });
@@ -21,14 +34,4 @@
     event.preventDefault();
   });
 
-  function DisplayCharacter(characters) {
-    // console.log(charactersObj);
-    // charactersObj.name = name;
-    // charactersObj.img = img;
-    // charactersObj.description = description;
-    // var currentCharacter = [];
-    // currentCharacter.push(this);
-    console.log('hey');
-  };
-  DisplayCharacter(characters);
 }(window));
